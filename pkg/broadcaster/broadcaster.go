71d93847f16410aa1ad16acfd620e1e37a9769f9
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-05-09 19:10:13 +0200

    Add Broker to the Broadcaster

--- pkg/broadcaster/broadcaster.go
+++ pkg/broadcaster/broadcaster.go
@@ -1,7 +1,9 @@
 package broadcaster
 
 import (
+	"github.com/Octops/gameserver-events-broadcaster/pkg/brokers"
 	"github.com/Octops/gameserver-events-broadcaster/pkg/controller"
+	"github.com/Octops/gameserver-events-broadcaster/pkg/events"
 	"github.com/Octops/gameserver-events-broadcaster/pkg/runtime/log"
 	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/rest"
@@ -11,13 +13,15 @@ import (
 type Broadcaster struct {
 	logger     *logrus.Entry
 	controller *controller.GameServerController
+	brokers.Broker
 }
 
-func New(config *rest.Config) (*Broadcaster, error) {
+func New(config *rest.Config, broker brokers.Broker) (*Broadcaster, error) {
 	logger := log.NewLoggerWithField("source", "broadcaster")
 
 	gsBroadcaster := &Broadcaster{
 		logger: logger,
+		Broker: broker,
 	}
 
 	gsController, err := controller.NewGameServerController(config, gsBroadcaster)
@@ -40,18 +44,48 @@ func (b *Broadcaster) Start() error {
 }
 
 func (b *Broadcaster) OnAdd(obj interface{}) error {
-	b.logger.Debug(obj)
+	if b.Broker == nil {
+		b.logger.Warn("broker is not available for the broadcaster, message will not be published")
+		return nil
+	}
+
+	event := &events.GameServerDeleted{
+		Message: &events.EventMessage{
+			Body: obj,
+		},
+	}
+
+	envelope, err := b.Broker.BuildEnvelope(event)
+	if err != nil {
+		b.logger.WithError(err).Error("error building envelope")
+	}
+
+	if err = b.Broker.SendMessage(envelope); err != nil {
+		b.logger.WithError(err).Error("error sending envelope")
+	}
+
+	b.logger.Debug(envelope)
 
 	return nil
 }
 
 func (b *Broadcaster) OnUpdate(oldObj interface{}, newObj interface{}) error {
+	if b.Broker == nil {
+		b.logger.Warn("a broker is not available for the broadcaster, message will not be published")
+		return nil
+	}
+
 	b.logger.Debug(oldObj, newObj)
 
 	return nil
 }
 
 func (b *Broadcaster) OnDelete(obj interface{}) error {
+	if b.Broker == nil {
+		b.logger.Warn("a broker is not available for the broadcaster, message will not be published")
+		return nil
+	}
+
 	b.logger.Debug(obj)
 
 	return nil
