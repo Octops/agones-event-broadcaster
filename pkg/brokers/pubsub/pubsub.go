ddafece65baf49c431ccdd70d5be0871194ff919
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-05-10 17:54:48 +0200

    Implement SendMessage

--- pkg/brokers/pubsub/pubsub.go
+++ pkg/brokers/pubsub/pubsub.go
@@ -1,7 +1,12 @@
 package pubsub
 
 import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/Octops/gameserver-events-broadcaster/pkg/events"
+	"github.com/sirupsen/logrus"
 )
 
 type Config struct {
@@ -14,16 +19,27 @@ type Config struct {
 
 type PubSubBroker struct {
 	*Config
+	*pubsub.Client
 }
 
-func NewPubSubBroker(config *Config) *PubSubBroker {
-	return &PubSubBroker{Config: config}
+func NewPubSubBroker(config *Config) (*PubSubBroker, error) {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, config.ProjectID)
+	if err != nil {
+		return nil, fmt.Errorf("error creating pubsub client for projectID %s: %v", config.ProjectID, err)
+	}
+
+	return &PubSubBroker{
+		Config: config,
+		Client: client,
+	}, nil
 }
 
-func (ps *PubSubBroker) BuildEnvelope(event events.Event) (*events.Envelope, error) {
+func (b *PubSubBroker) BuildEnvelope(event events.Event) (*events.Envelope, error) {
 	envelope := &events.Envelope{}
 	envelope.AddHeader("event_type", event.EventType())
-	envelope.AddHeader("project_id", ps.ProjectID)
+	envelope.AddHeader("project_id", b.ProjectID)
 	envelope.AddHeader("topic_id", "gameserver.events")
 
 	envelope.Message = event.(events.Message).Content()
@@ -31,6 +47,54 @@ func (ps *PubSubBroker) BuildEnvelope(event events.Event) (*events.Envelope, err
 	return envelope, nil
 }
 
-func (ps *PubSubBroker) SendMessage(envelope *events.Envelope) error {
-	panic("implement me")
+func (b *PubSubBroker) SendMessage(envelope *events.Envelope) error {
+	ctx := context.Background()
+
+	topicID, ok := GetTopicIDFromHeader(envelope)
+	if !ok {
+		return fmt.Errorf("topicID is not present on the envelope header")
+	}
+
+	topic := b.Client.Topic(topicID)
+
+	ok, err := topic.Exists(ctx)
+	if err != nil {
+		return fmt.Errorf("could not check if topic exists: %v", err)
+	}
+
+	if !ok {
+		return fmt.Errorf("topic %s for projectID %s does not exist", b.ProjectID, topicID)
+	}
+
+	msg, err := EncodedEnvelope(envelope)
+	if err != nil {
+		return fmt.Errorf("error encoding envelope: %v", err)
+	}
+
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data: msg,
+	})
+
+	// Block until the result is returned and a server-generated
+	// ID is returned for the published message.
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("Get: %v", err)
+	}
+
+	logrus.WithField("broker", "pubsub").Infof("message published topicID:\"%s\" messageID:\"%s\"", topicID, id)
+
+	return nil
+}
+
+func GetTopicIDFromHeader(envelope *events.Envelope) (string, bool) {
+	if topicID, ok := envelope.Header.Headers["topic_id"]; ok {
+		return topicID, true
+	}
+
+	return "", false
+}
+
+func EncodedEnvelope(envelope *events.Envelope) ([]byte, error) {
+	return json.Marshal(envelope)
 }
