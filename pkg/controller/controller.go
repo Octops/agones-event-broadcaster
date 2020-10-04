112298e82ced5a1f7de2fc25f42b598768c21f6c
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-10-04 21:38:19 +0200

    Disable reconcile

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,10 +1,8 @@
 package controller
 
 import (
-	"context"
 	"github.com/Octops/agones-event-broadcaster/pkg/events/handlers"
 	"github.com/sirupsen/logrus"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
@@ -141,24 +139,29 @@ func NewAgonesController(mgr manager.Manager, eventHandler handlers.EventHandler
 	return controller, nil
 }
 
+// Warning: This method is possible not meant to be used. It has a particular use case but the broadcaster uses a shorter
+// Sync period that triggers OnUpdate events. Right now this Reconcile function is useless for the broadcaster.
+// It should be explored in the future.
+
+// TODO: Evaluate is Reconcile should be made an argument for the Controller. Reconcile can be used for general uses cases
+// where control over very specific events matter. Right now it is just a STDOUT output.
 // Reconcile is called on every reconcile event. It does not differ between add, update, delete.
 // Its function is purely informative and events are handled back to the broadcaster specific event handlers.
 func (r *Reconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) {
-	ctx := context.Background()
+	//ctx := context.Background()
+	//obj := r.obj.DeepCopyObject()
+	//if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
+	//	if apierrors.IsNotFound(err) {
+	//		r.logger.WithField("type", reflect.TypeOf(obj).String()).Debugf("resource \"%s\" not found", req.NamespacedName)
+	//		return ctrl.Result{}, nil
+	//	}
+	//
+	//	r.logger.WithError(err).Error()
+	//
+	//	return reconcile.Result{}, err
+	//}
 
-	obj := r.obj.DeepCopyObject()
-	if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
-		if apierrors.IsNotFound(err) {
-			r.logger.WithField("type", reflect.TypeOf(obj).String()).Debugf("resource \"%s\" not found", req.NamespacedName)
-			return ctrl.Result{}, nil
-		}
-
-		r.logger.WithError(err).Error()
-
-		return reconcile.Result{}, err
-	}
-
-	r.logger.Debugf("OnReconcile: %s (%s)", req.NamespacedName, reflect.TypeOf(obj).String())
+	//r.logger.Debugf("OnReconcile: %s (%s)", req.NamespacedName, reflect.TypeOf(obj).String())
 
 	return reconcile.Result{}, nil
 }
